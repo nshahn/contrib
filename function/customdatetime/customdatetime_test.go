8102384284738d3068ab1b0f373af581db605b06
Nikhil Shah <nshah@tibco.com>
2019-11-18 13:53:40 +0530

    Added new custom datetime function for returning timestamp in ms for cussrent date time

---
+++ function/customdatetime/customdatetime_test.go
@@ -0,0 +1,20 @@
+package customdatetime
+
+import (
+	"fmt"
+	"testing"
+	"github.com/project-flogo/core/data/expression/function"
+)
+
+var in = &fnDateTimeToTimeStamp{}
+
+func init() {
+	function.ResolveAliases()
+}
+
+func TestInt64Sample(t *testing.T) {
+	final, err := in.Eval()
+	assert.Nil(t, err)
+	//assert.Equal(t, int(579), final)
+	fmt.Printf("%v\n", final)
+}
